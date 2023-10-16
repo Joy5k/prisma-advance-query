003562f7143591e3b02260325dc9723ea9d42e68
Joy <mmehedihasanjoyv@gmail.com>
2023-10-16 23:02:59 +0600

    created user

---
+++ src/app/api/createUser/route.js
@@ -0,0 +1,21 @@
+import { PrismaClient } from "@prisma/client";
+import { NextResponse } from "next/server";
+
+const prisma = new PrismaClient
+export async function POST(req, res) {
+    try {
+        await prisma.user.create({
+            data: {
+                firstName:"Mehedi",
+                middleName:"Hasan",
+                lastName:"Joy",
+                mobile:"01923588531",
+                email: "mmehedihasanjoyv@gmail.com",
+                password:"123456"
+         }
+     })
+    return NextResponse.json({status: 200,message:"success"})
+ } catch (error) {
+    return NextResponse.json({message:"Fail to create user ",error: error.name,ErrorDetails:error});
+ }    
+}
